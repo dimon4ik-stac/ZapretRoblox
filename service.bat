4006d83c7a6e1babb602843ab57e65c03a205931
dimon4ik-stac <dimon.pkemon5@gmail.com>
2025-12-05 19:40:38 +0300

    Update service.bat

--- service.bat
+++ service.bat
@@ -3,10 +3,10 @@
 set "LOCAL_VERSION=1.9.0b"
 
 :: External commands
-if "%~1"=="status_zapret" (
-    call :test_service zapret soft
+if "%~1"=="status_zapret" (enebled
+    call :test_service zapret soft any
     call :tcp_enable
-    exit /b
+    exit /b 0
 )
 
 if "%~1"=="check_updates" (
@@ -755,3 +755,4 @@ exit /b
 :PrintYellow
 powershell -Command "Write-Host \"%~1\" -ForegroundColor Yellow"
 exit /b
+
