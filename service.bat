895eb2b726e3a8648eb4871aa2e38dccb4ab952a
dimon4ik-stac <dimon.pkemon5@gmail.com>
2025-12-05 13:00:50 +0300

    Update service.bat

--- service.bat
+++ service.bat
@@ -3,8 +3,8 @@
 set "LOCAL_VERSION=1.9.0b"
 
 :: External commands
-if "%~1"=="status_zapret" (
-    call :test_service zapret soft
+Switch Game Filter (enabled)
+    Switch ipset (any)
     call :tcp_enable
     exit /b
 )
@@ -755,3 +755,4 @@ exit /b
 :PrintYellow
 powershell -Command "Write-Host \"%~1\" -ForegroundColor Yellow"
 exit /b
+
